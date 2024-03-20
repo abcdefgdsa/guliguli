43467dcaa8a25049b3f318a919b081659e1be8ea
aimeng <1576503876@qq.com>
2024-03-21 00:29:43 +0800

    三级分类功能的查询

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,34 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //先拿到所有的分类数据
+        List<CategoryEntity> all = list();
+        //组装成父子的树形结构
+        List<CategoryEntity> collect = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == 0;
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(selectChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((cate1, cate2) -> {
+            return (cate1.getSort() == null ? 0 : cate1.getSort()) - (cate2.getSort() == null ? 0 : cate2.getSort());
+        }).collect(Collectors.toList());
+        return collect;
+    }
+
+    private List<CategoryEntity> selectChildren(CategoryEntity category, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == category.getCatId();
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(selectChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((cate1, cate2) -> {
+            return (cate1.getSort() == null ? 0 : cate1.getSort()) - (cate2.getSort() == null ? 0 : cate2.getSort());
+        }).collect(Collectors.toList());
+        return children;
     }
 
 }
